cfddb30fc216454ea999fc3587e09c6102ccc15b
xChygyNx <Chykha110@gmail.com>
2019-11-13 19:37:58 +0300

    detect label & read name of all labels in the filemake

--- folder_of_example/champs/comment.s
+++ folder_of_example/champs/comment.s
@@ -2,3 +2,5 @@
 .comment "testtesttest"
 
 com1:	and	r1, %3, r2
+com17:	and	r1, %3, r2
+abra-cadabra:	and	r1, %3, r2
--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: pcredibl <pcredibl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 16:24:27 by pcredibl          #+#    #+#             */
-/*   Updated: 2019/11/13 18:27:21 by pcredibl         ###   ########.fr       */
+/*   Updated: 2019/11/13 18:50:08 by pcredibl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,31 @@ static void detect_label(t_champ *champ, char **data)
 	int		i;
 
 	i = 0;
-	while (!ft_isspace((*data)[i]) && (*data)[i] != ':')
+	while ((*data)[i] && !ft_isspace((*data)[i]) && (*data)[i] != ':')
 		i++;
 	if ((*data)[i] == ':')
 	{
 		champ->labels = add_label2end(champ->labels, ft_strsub((const char*)*data, 0, i));
 		*data += i + 1;
-	}	
-	//ft_printf("label name = %s\n", champ->labels->name);
+	}
+	//ft_printf("label = %p\n", champ->labels);	
+	//champ->labels ? ft_printf("label name = %s\n", champ->labels->name) : 0;
 	//ft_printf("data = %s\n", *data);
 }
 
 void	parse_instr(t_champ *champ, char *filedata)
 {
-	int		offset;
+	int		i;
 
-	detect_label(champ, &filedata);
+	while (*filedata)
+	{
+		detect_label(champ, &filedata);
+		if (*filedata++ == '\n')
+			detect_label(champ, &filedata);
+	}
+	while (champ->labels)
+	{
+		ft_printf("label = %s\n", champ->labels->name);
+		champ->labels = champ->labels->next;
+	}
 }
